4dead8fd666d1db41a59117c14fe0750c37ff85b
Rafael S. Zanella <rafaels.zanella@gmail.com>
2020-01-08 02:22:14 -0300

    feat(app, style, index): adicionar imagem e tratar elementos do objeto da api.

--- js/app.js
+++ js/app.js
@@ -1,11 +1,50 @@
 let inputPokemonNumber = document.getElementById('input-pokemon-number');
-let labelPokemonName = document.getElementById('label-pokemon-name');
+//let labelID = document.getElementById
+let labelName = document.getElementById('label-pokemon-name');
+let picture = document.getElementById('picture');
+// let labelHeight
+// let labelWeight
+// let types
+// let
+
+
+let pokemon = {
+    id: 0,
+    name: "",
+    height: 0,
+    weight: 0,
+    types: [],
+    stats: [],
+    sprites: { front_default: "" }
+}
+
 
 inputPokemonNumber.addEventListener("blur", () => {
-    let pokeAPI = fetch(`https://pokeapi.co/api/v2/pokemon/${inputPokemonNumber.value}/`);
-    labelPokemonName.innerHTML = 'Pesquisando...';
+    getPokemonInfo(inputPokemonNumber.value);
+});
+
+let getPokemonInfo = (id) => {
+    let pokeAPI = fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`);
+    //labelPokemonName.innerHTML = 'Pesquisando...';
     pokeAPI
         .then(data => data.json())
-        .then(data => labelPokemonName.innerHTML = (data.name))
-        .catch(data => labelPokemonName.innerHTML = 'Digite um id valido!')
-});
+        .then(data => {
+            pokemon = data
+        })
+        .then(() => {
+            console.log(`
+            id: ${pokemon.id} 
+            nome: ${pokemon.name} 
+            height: ${pokemon.height} 
+            weight: ${pokemon.weight} 
+            types: ${pokemon.types.map(x => x.type.name)} 
+            stats: ${pokemon.stats}
+            sprites: ${pokemon.sprites.front_default}
+            `);
+
+            picture.src = ""+pokemon.sprites.front_default;
+
+
+        })
+        .catch(data => labelName.innerHTML = 'Digite um id valido!')
+}
